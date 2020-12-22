50be247cf127224639286581eaad87925c8a00d4
Chema <chemamola@gmail.com>
2020-12-22 21:46:44 +0100

    [+] spark initializer

--- src/main/scala/org/dark/code/Application.scala
+++ src/main/scala/org/dark/code/Application.scala
@@ -1,9 +1,25 @@
 package org.dark.code
 
+import org.apache.spark.sql.SparkSession
+
 /**
  * Hello world!
  *
  */
 object Application extends App {
-  println( "Hello World!" )
+
+  /**
+   * function that initialize the spark session object.
+   * (get or create).
+   *
+   * @param appName app name
+   * @return        spark session
+   */
+  private def sparkInicializer(appName: String): SparkSession = {
+    SparkSession.builder().appName(appName).getOrCreate()
+  }
+
+  override def main(args: Array[String]): Unit = {
+
+  }
 }
